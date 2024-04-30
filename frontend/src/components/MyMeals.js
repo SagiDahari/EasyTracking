3c1f16e596a57078d4e385af169d5ca09169b085
Sagi Dahari <sagidahari7@gmail.com>
2024-04-30 16:44:50 +0300

    fixed frontend dockerfile

--- frontend/src/components/MyMeals.js
+++ frontend/src/components/MyMeals.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import MealColumn from "./MealColumn";
 import { useNavigate } from "react-router-dom";
 
